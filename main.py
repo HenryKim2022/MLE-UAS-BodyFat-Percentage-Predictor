f79b2464960ce0591b8cd88ce392e9f77ae14e18
HenryKim2022 <119053402+HenryKim2022@users.noreply.github.com>
2022-11-27 03:14:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -517,16 +517,15 @@ def footer2():
                 color: black;
                 text-align: center;
             }
+        </style>    
         <style>
             # MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
         </style>
-        </style>
-            <div class="footer">
-            <b>Made with</b>: Python<b> Developed by</b>: <a style='display: block; text-align: center;' href="https://bit.ly/3F5BzMm/" target="_blank">Hendri.</a><br>
+        <div class="footer">
+            <b>Made with</b>: Python<b> Developed by</b>: <a style='display: block; text-align: center;' href="https://bit.ly/3F5BzMm/" target="_blank">Hendri.</a>
             © Copyright 2022<br>
-            All rights reserved. Powered by:<a style='display: block; text-align: center;' href="https://streamlit.io/" target="_blank"> Streamlit.<br>
-        </div>
+            All rights reserved. Powered by:<a style='display: block; text-align: center;' href="https://streamlit.io/" target="_blank"> Streamlit.
         </div>
     """
     content_htmlMarkdown(footerTxt)
